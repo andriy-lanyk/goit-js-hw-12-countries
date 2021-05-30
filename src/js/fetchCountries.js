3f875d2d54e51a038feba9b98b9190141754338f
andriy-lanyk <LAM1988@ukr.net>
2021-05-31 01:31:50 +0300

    Catch error

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,23 +1,30 @@
+import { error } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+
 export default function fetchCountries(searchQuery) {
-  fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
-    .then(response => response.json())
-    .then(data => {
-      if (data.length >= 2 && data.length <= 10) {
-        console.log('data.length: ', data.length);
-        return data.map(c => {
-          c.name;
-        });
-      }
-      if (data.length === 1) {
-        return data.map(c => {
-          c.name;
-          c.flag;
-          c.capital;
-          c.population;
-          c.languages.map(language => {
-            language.name;
+  return fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
+    .then(response => {
+      if (!response.ok) {
+        if (response.status === 404) {
+          error({
+            text: 'There is no country with such name. Please enter a correct query!',
+            width: '420px',
+            minHeight: '20px',
+            maxTextHeight: null,
+            delay: 4000,
           });
-        });
+        } else {
+          error({
+            text: 'We have problem with a server. Please try again!',
+            width: '420px',
+            minHeight: '20px',
+            maxTextHeight: null,
+            delay: 4000,
+          });
+        }
       }
-    });
+      return response.json();
+    })
+    .then(data => data);
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,51 @@
 import debounce from 'lodash.debounce';
-import countriesList from '..templates/countries-list.hbs';
+import { error } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+import countriesListTpl from '../../src/templates/countries-list.hbs';
+import countryArticleTpl from '../../src/templates/country-article.hbs';
 import '../sass/main.scss';
-
 import fetchCountries from './fetchCountries.js';
 
-const input = document.querySelector('[name="countris"]');
+const input = document.querySelector('[name="countries"]');
+const countriesList = document.querySelector('.js-list');
 
-input.addEventListener('input', debounce(changeCountry, 500));
+input.addEventListener('input', debounce(chooseCountry, 500));
 
-function changeCountry(e) {
+function chooseCountry(e) {
   let searchQuery = e.target.value;
+  if (searchQuery.trim() === '') {
+    countriesList.innerHTML = '';
+    return;
+  }
 
-  fetchCountries(searchQuery);
+  fetchCountries(searchQuery).then(data => {
+    if (data.length >= 2 && data.length <= 10) {
+      return createCountriesList(data);
+    }
+    if (data.length === 1) {
+      return createCountryArticle(data);
+    }
+    if (data.length > 10) {
+      return notification();
+    }
+  });
+}
+
+function createCountriesList(data) {
+  countriesList.innerHTML = countriesListTpl(data);
+}
+
+function createCountryArticle(data) {
+  countriesList.innerHTML = countryArticleTpl(data);
+}
+
+function notification() {
+  error({
+    text: 'Too many matches found. Please enter a more specific query!',
+    width: '420px',
+    minHeight: '20px',
+    maxTextHeight: null,
+    delay: 4000,
+  });
 }
